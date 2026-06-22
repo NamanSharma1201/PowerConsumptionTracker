dc8b84609335ea078accabbb4b9ec70593f7a783
Naman Sharma <naman.sharma.122003@gmail.com>
2026-06-22 07:08:37 +0530

    user service implementation

---
+++ user-service/src/main/java/com/ncorp/user_service/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.ncorp.user_service.controller;
+
+import com.ncorp.user_service.dto.UserDto;
+import com.ncorp.user_service.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@Slf4j
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+        UserDto created = userService.createUser(userDto);
+        return new  ResponseEntity<>(created, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id){
+        UserDto userDto = userService.getUserById(id);
+        if(userDto == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody UserDto userDto){
+        try {
+            userService.updateUser(id, userDto);
+            return new ResponseEntity<>("User updated successfully", HttpStatus.OK);
+        }catch (IllegalArgumentException e){
+            return new ResponseEntity<>("User not found", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id){
+        try {
+            userService.deleteUser(id);
+            return ResponseEntity.noContent().build();
+        }catch (IllegalArgumentException e){
+            return new ResponseEntity<>("User not found", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
---
+++ user-service/src/main/java/com/ncorp/user_service/service/UserService.java
@@ -0,0 +1,81 @@
+package com.ncorp.user_service.service;
+
+import com.ncorp.user_service.dto.UserDto;
+import com.ncorp.user_service.entity.User;
+import com.ncorp.user_service.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public UserDto createUser(@RequestBody UserDto userDto){
+        log.info("Creating user: {}", userDto);
+        final User createdUser =  User.builder()
+                .name(userDto.getName())
+                .surname(userDto.getSurname())
+                .email(userDto.getEmail())
+                .address(userDto.getAddress())
+                .alerting(userDto.isAlerting())
+                .energyAlertingThreshold(userDto.getEnergyAlertingThreshold())
+                .build();
+
+        final User saved = userRepository.save(createdUser);
+
+        return toDto(saved);
+    }
+
+
+    private UserDto toDto(User user){
+        return UserDto.builder()
+                .name(user.getName())
+                .surname(user.getSurname())
+                .email(user.getEmail())
+                .address(user.getAddress())
+                .alerting(user.isAlerting())
+                .energyAlertingThreshold(user.getEnergyAlertingThreshold())
+                .build();
+    }
+
+    public UserDto getUserById(Long id) {
+        log.info("Retrieving user by id: {}", id);
+        final Optional<User> user = userRepository.findById(id);
+        return user.map(this::toDto).orElse(null);
+    }
+
+    public void updateUser(Long id, UserDto userDto) {
+        log.info("Updating user: {}", userDto);
+        final Optional<User> user = userRepository.findById(id);
+        if(user.isEmpty()) {
+            throw new IllegalArgumentException("User not found");
+        }
+        user.get().setName(userDto.getName());
+        user.get().setSurname(userDto.getSurname());
+        user.get().setEmail(userDto.getEmail());
+        user.get().setAddress(userDto.getAddress());
+        user.get().setAlerting(userDto.isAlerting());
+        user.get().setEnergyAlertingThreshold(userDto.getEnergyAlertingThreshold());
+        userRepository.save(user.get());
+    }
+
+    public void deleteUser(Long id) {
+        log.info("Deleting user: {}", id);
+        try{
+            userRepository.deleteById(id);
+        }catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("User not found");
+        }
+    }
+}
